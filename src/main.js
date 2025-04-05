e99cc028f0036612052ae0a30066fc88369995b6
04stefke <jovanovics399@yahoo.com>
2025-04-05 16:09:56 +0100

    implemented logic for the map of the ip address wanted, also made a default location of new york for the map and added marker for the location

--- src/main.js
+++ src/main.js
@@ -7,24 +7,44 @@ const timezone = document.getElementById("timezone-data");
 const isp = document.getElementById("isp-data");
 const input = document.getElementById("ipInput");
 
+let lat = 40.7128;
+let lng = -74.006;
+const map = L.map("map").setView([lat, lng], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+	attribution:
+		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+let marker = L.marker([lat, lng]).addTo(map)
+
 const fetchData = async (term) => {
 	try {
+		lat = [];
+		lng = [];
 		const response = await axios.get(
 			`https://geo.ipify.org/api/v2/country,city?apiKey=at_GvJkFQiCUEBocM7CTQPZ3RPjJth1E&ipAddress=${term}`
 		);
 		const res = response.data;
 		console.log(res);
+
 		location.innerText = `${res.location.city}, ${res.location.country} ${res.location.postalCode}`;
 		timezone.innerText = res.location.timezone;
 		isp.innerText = res.isp;
 		ipAddressData.innerText = res.ip;
+
+		lat = res.location.lat;
+		lng = res.location.lng;
+
+		map.setView([lat, lng], 13);
+		marker.setLatLng([lat, lng])
 	} catch (error) {
 		console.error("Error fetching data:", error);
 	}
 };
 searchIp.addEventListener("click", (e) => {
-    e.preventDefault()
-	fetchData(input.value);  
+	e.preventDefault();
+	fetchData(input.value);
 });
 
 // {ip: '178.220.45.214', location: {…}, as: {…}, isp: 'TSSRB - Telekom Srbija'}
